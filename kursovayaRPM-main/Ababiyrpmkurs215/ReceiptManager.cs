c6568d3f329aff1e7f02784b7a23476639ac0f0b
F1ner <123871318+F1ner@users.noreply.github.com>
2024-05-17 12:38:52 +0300

    Add files via upload

---
+++ kursovayaRPM-main/Ababiyrpmkurs215/ReceiptManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiyrpmkurs215
+{
+    class ReceiptManager
+    {
+        private List<Transaction> transactions = new List<Transaction>();
+        string filePath = @"C:\Users\Kab-35-6\Desktop\kursovayaRPM-main\Ababiyrpmkurs215\bin\Debug\receipt.bin";
+        public void AddTransaction()
+        {
+            try
+            {
+                
+                Console.Write("Введите имя оператора: ");
+                string currentOperator = Console.ReadLine();
+                Console.Write("Введите номер телефона: ");
+                int phoneNumber =Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите сумму наличных: ");
+                decimal amount = decimal.Parse(Console.ReadLine());
+                transactions.Add(new Transaction(currentOperator, phoneNumber, amount));
+                
+
+                SaveToFile();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);AddTransaction();
+            }
+            finally { 
+            LoadFromFile();
+                }
+           
+        }
+
+        public void Delete()
+        {
+            LoadFromFile();
+            transactions.Clear();
+            SaveToFile();
+        }
+        public void PrintReceipts()
+        {
+            LoadFromFile();
+            foreach (var transaction in transactions)
+            {
+                Console.WriteLine(transaction);
+            }
+        }
+
+        public void SaveToFile()
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(fs, transactions);
+            }
+        }
+
+        public void LoadFromFile()
+        {
+            if (File.Exists(filePath))
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    transactions = (List<Transaction>)formatter.Deserialize(fs);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден.");
+            }
+        }
+    }
+}
